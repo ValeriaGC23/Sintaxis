63a5c16588b38d082f8ec37937a3d56fd55a435a
ValeriaGC23 <valegc0820@gmail.com>
2025-07-30 09:59:01 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,122 +1,55 @@
-public class Ej1A {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6};
-        int suma = 0;
+import java.util.Scanner;
+import java.util.Arrays;
 
-        // Recorremos el arreglo con un for básico
-        for (int i = 0; i < nums.length; i++) {
-            // Si el número es par, lo sumamos
-            if (nums[i] % 2 == 0) {
-                suma += nums[i];
-            }
+public class Main {
+    public static void main(String[] args) {
+
+        // LEER EN CONSOLA
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine(); //Lee linea completa en consola
+        String palabra = sc.next(); //Lee palabra
+        int edad = sc.nextInt(); // Lee enteros
+
+        // IMPRIMIR
+        System.out.println(palabra); //Imprime y agrega salto de linea
+        System.out.print(palabra + "Hola"); //Imprime y no agrega salto de linea
+
+        //If
+        if (edad >= 18) {
+        }
+        else {
         }
 
-        System.out.println("Suma de pares: " + suma);  // Resultado: 12
+        //FOR
+        for (int i = 0; i < 10; i++) {
+        }
+
+        // WHILE
+        while (i < 10) {
+        }
+
+        // CONVERSION DE TIPOS
+        int num = 100;
+        float numdecimal = num; // De entero a decimal
+
+        int nume = 15;
+        String texto = edad + "";  // De num a string
+
+        String palabra2 = "1234";
+        int n = Integer.parseInt(palabra2); //De string a int
+        double d = Double.parseDouble(palabra2); //De string a double
+
+        //Redondear
+        float Pi = 3.998734f;
+        System.out.printf("%.2f", Pi);
+
+        // ARREGLOS
+        int [] numeros  = new int [10]; //Inicializar arreglo
+        int [] numeros2 = {1, 2, 3, 4, 5, 6, 7, 8, 9}; //Declarar arreglo
+
+        System.out.println(Arrays.toString(numeros)); //Imprimir arreglo
+        System.out.println(numeros2.length); //Imprimir num de elementos que tiene un array
+
     }
 }
 
-
-public class Ej1B {
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6};
-        int suma = 0;
-
-        // Recorremos el arreglo
-        for (int i = 0; i < nums.length; i++) {
-            int valor = nums[i];  // Guardamos el número en una variable
-            if (valor % 2 == 0) {
-                suma = suma + valor;
-            }
-        }
-
-        System.out.println("Suma de pares: " + suma);  // Resultado: 12
-    }
-}
-
-
-public class Ej2A {
-    public static void main(String[] args) {
-        int[] nums = {4, 1, 2, 1, 2};
-
-        for (int i = 0; i < nums.length; i++) {
-            boolean unico = true;
-
-            for (int j = 0; j < nums.length; j++) {
-                // Evitamos comparar la misma posición
-                if (i != j && nums[i] == nums[j]) {
-                    unico = false;
-                    break;
-                }
-            }
-
-            if (unico) {
-                System.out.println("Número único: " + nums[i]); // Resultado: 4
-                break;  // Solo uno cumple, así que salimos
-            }
-        }
-    }
-}
-
-
-public class Ej2B {
-    public static void main(String[] args) {
-        int[] nums = {4, 1, 2, 1, 2};
-
-        for (int i = 0; i < nums.length; i++) {
-            int contador = 0;
-
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    contador++;
-                }
-            }
-
-            if (contador == 1) {
-                System.out.println("Número único: " + nums[i]); // Resultado: 4
-                break;
-            }
-        }
-    }
-}
-
-public class Ej3A {
-    public static void main(String[] args) {
-        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
-
-        // Buscamos del 1 hasta el tamaño del arreglo
-        for (int i = 1; i <= nums.length; i++) {
-            boolean encontrado = false;
-
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == i) {
-                    encontrado = true;
-                    break;
-                }
-            }
-
-            if (!encontrado) {
-                System.out.println("Falta el número: " + i);
-            }
-        }
-    }
-}
-
-public class Ej3B {
-    public static void main(String[] args) {
-        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
-
-        for (int i = 1; i <= nums.length; i++) {
-            int contador = 0;
-
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == i) {
-                    contador++;
-                }
-            }
-
-            if (contador == 0) {
-                System.out.println("Falta el número: " + i);
-            }
-        }
-    }
-}
